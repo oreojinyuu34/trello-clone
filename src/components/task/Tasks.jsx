7c1056ace3ed7a538614605f29a1bb6366aa0fd1
atsushi <77239082+oreojinyuu34@users.noreply.github.com>
2023-07-16 23:44:14 +0900

    タスクカード作成

--- src/components/task/Tasks.jsx
+++ src/components/task/Tasks.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { OneTask } from "./OneTask";
 
-export const Tasks = () => {
-  return <div></div>;
+export const Tasks = ({ taskList }) => {
+  return (
+    <div>
+      {taskList.map((task) => (
+        <div>
+          <OneTask task={task} />
+        </div>
+      ))}
+    </div>
+  );
 };
--- src/components/task/input/TaskAddInput.jsx
+++ src/components/task/input/TaskAddInput.jsx
@@ -1,5 +1,39 @@
 import React from "react";
 
-export const TaskAddInput = () => {
-  return <div></div>;
+export const TaskAddInput = ({
+  inputText,
+  setInputText,
+  setTaskList,
+  taskList,
+}) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e);
+    //カードを追加する。
+    setTaskList([
+      ...taskList,
+      {
+        text: inputText,
+      },
+    ]);
+    setInputText("");
+    // console.log(...taskList);
+    // console.log(inputText);
+  };
+  const handleChange = (e) => {
+    setInputText(e.target.value);
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="add a task"
+          className="taskAddInput"
+          onChange={handleChange}
+          value={inputText}
+        />
+      </form>
+    </div>
+  );
 };
